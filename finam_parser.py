431250241932f1b4431f545a84ade57872871946
Andrey Potseluev <potseluevam@studemt.bmstu.ru>
2024-12-14 17:08:07 +0300

    save news to .csv files

--- finam_parser.py
+++ finam_parser.py
@@ -1,15 +1,14 @@
 import asyncio
-from playwright.async_api import async_playwright, expect
-import os 
+from playwright.async_api import async_playwright
+import pandas as pd
 
 base_url = 'https://www.finam.ru'
 ticket = 'ydex'
-amount = 10
+amount = 3
 
 async def async_work():
     async with async_playwright() as p:
         browser = await p.chromium.launch(
-            channel='chrome',
             headless=False
         )
         page = await browser.new_page()
@@ -19,24 +18,33 @@ async def async_work():
         for d in div:
             s.append(await d.get_attribute("href"))
         urls = s[::2]
-        if not os.path.exists("./data/" + ticket): 
-            os.makedirs("./data/" + ticket)
+        
+        news_data = []  # Список для хранения новостей
         i = 0
         for url in urls:
             if i == amount:
                 break
             i += 1
-            if(os.path.exists("./data/" + ticket + url[url.find('item/') + 4:-1] + '.txt')):
-                continue
+
             new_page = await browser.new_page()
-            await page.goto(base_url + url)
-            paragraphs = await page.locator('p').filter(has_not_text="Дизайн — «Липка и Друзья»").all()
-            with open('./data/' + ticket  + url[url.find('item/') + 4:-1] + '.txt', 'a', encoding='utf-8') as f:
-                for paragraph in paragraphs:
-                    s = await paragraph.inner_text()
-                    if (s == ('При полном или частичном использовании материалов ссылка на\xa0Finam.ru\xa0обязательна. Подробнее об\xa0использовании информации и\xa0котировок. Редакция не\xa0несет ответственности за\xa0достоверность информации, опубликованной в\xa0рекламных объявлениях.\xa0\xa018+')):
-                        break
-                    f.write(s)
+            await new_page.goto(base_url + url)
+            paragraphs = await new_page.locator('p').filter(
+                has_not_text="Дизайн — «Липка и Друзья»"
+            ).all()
+            news_text = ""
+            for paragraph in paragraphs:
+                s = await paragraph.inner_text()
+                if s == ('При полном или частичном использовании материалов ссылка на Finam.ru обязательна. Подробнее об использовании информации и котировок. Редакция не несет ответственности за достоверность информации, опубликованной в рекламных объявлениях.  18+'):
+                    break
+                news_text += s + "\n"
+
+            news_data.append({"url": base_url + url, "content": news_text})
+            print(i)
+            await new_page.close()
+
+        news_df = pd.DataFrame(news_data)
+        news_df.to_csv(f"./data/{ticket}_news.csv", index=False, encoding='utf-8')
+
         await browser.close()
 
 asyncio.run(async_work())
